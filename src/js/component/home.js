7156b09b690a4c2471764a1fd9a17a3fb3016e33
TAPIAR7 <rtapiatec@gmail.com>
2021-03-10 20:20:18 +0000

    Traffic light version 2 completed

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+let currentTarget = "red";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("");
+	// setColor(currentTarget);
+	const HandleColor = e => {
+		let color = e.target.id;
+		setColor(color);
+	};
+	let newClassR = "";
+	let newClassY = "";
+	let newClassG = "";
+
+	if (color === "red") {
+		newClassR = "bg-danger rounded-circle light selected";
+	} else {
+		newClassR = "bg-danger rounded-circle light";
+	}
+	if (color === "yellow") {
+		newClassY = "bg-warning rounded-circle light selected";
+	} else {
+		newClassY = "bg-warning rounded-circle light";
+	}
+	if (color === "green") {
+		newClassG = "bg-success rounded-circle light selected";
+	} else {
+		newClassG = "bg-success rounded-circle light";
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-dark d-flex justify-content-center">
+			<div id="red" className={newClassR} onClick={HandleColor}></div>
+			<div id="yellow" className={newClassY} onClick={HandleColor}></div>
+			<div id="green" className={newClassG} onClick={HandleColor}></div>
 		</div>
 	);
 }
